5ae0648942ad1eea99349ef08f9c05204d63af9a
Alfonso <alfonso.hernandez>
2015-12-23 13:54:55 -0600

    Initial scaffolding.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # angu-twitch
-Twitch search client using angular 1.x
+
+This is the intial scaffolding for a twitch client made in angular.
+
+## Build & development
+
+Run `grunt` for building and `grunt serve` for preview.
+
+## Testing
+
+Running `grunt test` will run the unit tests with karma.
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name anguTwitchApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the anguTwitchApp
+ */
+angular.module('anguTwitchApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
